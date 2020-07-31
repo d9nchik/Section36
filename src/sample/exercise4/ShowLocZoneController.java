6e529a157e2a13b2c97ff9f92b9d665b038fdf54
d9nich <dan.galaiko@gmail.com>
2020-07-31 17:42:52 +0300

    Beta version has been done.

---
+++ src/sample/exercise4/ShowLocZoneController.java
@@ -0,0 +1,34 @@
+package sample.exercise4;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+import java.util.Locale;
+import java.util.TimeZone;
+import java.util.stream.Stream;
+
+public class ShowLocZoneController {
+    @FXML
+    private TextArea textArea;
+
+    private String locales, zones;
+
+
+    @FXML
+    public void initialize() {
+        locales = Stream.of(Locale.getAvailableLocales()).map(Locale::getDisplayName).filter(e -> !e.isEmpty()).collect(
+                StringBuilder::new, (c, e) -> c.append(e).append("\n"), StringBuilder::append).toString();
+        zones = Stream.of(TimeZone.getAvailableIDs()).collect(
+                StringBuilder::new, (c, e) -> c.append(e).append("\n"), StringBuilder::append).toString();
+    }
+
+    @FXML
+    public void showLocales() {
+        textArea.setText(locales);
+    }
+
+    @FXML
+    public void showZones() {
+        textArea.setText(zones);
+    }
+}
